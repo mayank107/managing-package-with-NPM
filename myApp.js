b691b705c608e4d8ba06ad7309e5fc092268849f
Mayank <mayank_kothari@icloud.com>
2020-10-20 21:46:00 +0530

    json file entry

--- myApp.js
+++ myApp.js
@@ -20,6 +20,11 @@ console.log("Hello World")
 
 
 /** 5) serve JSON on a specific route */
+app.get('/json',(req,res)=>{
+    res.json(
+        {"message":"Hello json"}
+        );
+})
 
 
 /** 6) Use the .env file to configure the app */
